ff9f98287653ae395e341d35435bea9f7b74aab7
Real-Memezz <104913540+Real-Memezz@users.noreply.github.com>
2023-03-03 13:54:46 +0530

    Update by chat GPT again
    1: Added a limit on the number of letters that can be guessed wrongly
    2 :Removed the "used letters" display
    3: Added a "play again" option at the end of the game
    4: Made the code more modular by creating functions for different parts of the game

--- hangman.py
+++ hangman.py
@@ -1,136 +1,129 @@
 import random
 
-# List of words for the game
-words = ["python", "java", "ruby", "javascript", "html", "css"]
+WORDS = ["python", "java", "ruby", "javascript", "html", "css"]
+MAX_TRIES = 7
 
-# Function to choose a random word from the list
 def choose_word(words):
     return random.choice(words)
 
-# Function to print the current state of the hangman
-def print_hangman(tries):
-    stages = [  # Final stage: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # Stage 6: head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # Stage 5: head, torso, both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # Stage 4: head, torso, one arm, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # Stage 3: head, torso, one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # Stage 2: head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # Stage 1: head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # Initial stage: empty gallows
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
+def get_guess():
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) != 1:
+            print("Please enter a single letter.")
+        elif not guess.isalpha():
+            print("Please enter a letter.")
+        else:
+            return guess
+
+def display_word(word, guesses):
+    for letter in word:
+        if letter in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print()
+
+def display_hangman(tries):
+    stages = [
+        """
+           +---+
+           |   |
+               |
+               |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+               |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+           |   |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\  |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\  |
+          /    |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\  |
+          / \  |
+               |
+        =========
+        """
     ]
     print(stages[tries])
 
-# Function to check if the player has won
-def check_win(word, guesses):
-    for letter in word:
-        if letter not in guesses:
-            return False
-    return True
-
-# Main function to run the game
 def play_game():
     print("Welcome to Hangman!")
-    word = choose_word(words)
-    word_letters = set(word)
-    used_letters = set()
-    tries = 7
+    word = choose_word(WORDS)
+    guesses = set()
+    wrong_guesses = 0
 
-    while tries > 0:
-        print_hangman(tries)
-        print("Used letters:", " ".join(used_letters))
-        word_list = [letter if letter in used_letters else "-" for letter in word]
-        print("Current word:", " ".join(word_list))
+    while True:
+        display_hangman(wrong_guesses)
+        display_word(word, guesses)
 
-        guess = input("Guess a letter: ").lower()
-        if guess in used_letters:
-            print("You already guessed that letter!")
-        elif guess in word_letters:
+        if wrong_guesses == MAX_TRIES:
+            print("Sorry, you ran out of guesses. The word was:", word)
+            break
+
+        if set(word) == guesses:
+            print("Congratulations, you guessed the word!")
+            break
+
+        guess = get_guess()
+        if guess in guesses:
+            print("You already guessed that letter.")
+        elif guess in word:
             print("Good guess!")
-            used_letters.add(guess)
-            if check_win(word, used_letters):
-                print_hangman(0)
-                print("Congratulations! You win!")
-                break
+            guesses.add(guess)
         else:
             print("Sorry, that letter is not in the word.")
-            used_letters.add(guess)
-            tries -= 1
+            wrong_guesses += 1
 
-    if tries == 0:
-        print_hangman(0)
-        print("Sorry, you ran out of tries. The word was", word)
+    play_again = input("Would you like to play again? (y/n)").lower()
+    if play_again == "y":
+        play_game()
+    else:
+        print("Thanks for playing!")
 
-# Call the main function to start the game
 play_game()
